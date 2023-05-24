c4be7b3a1df724da2e8260707a8a45069a7455b6
Andrzej Dabrowski <a.r.dabrowski@gmail.com>
2023-05-24 17:49:33 +0200

    changed copy

--- src/HeaderClientComponent.tsx
+++ src/HeaderClientComponent.tsx
@@ -3,7 +3,7 @@ import React, {useEffect} from 'react'
 
 export function HeaderClientComponent(props: { title: string }) {
   useEffect(() => {
-    console.log('some useEffect in BaseConfigRenderer');
+    console.log('some useEffect in HeaderClientComponent');
   })
 
   return <div>
